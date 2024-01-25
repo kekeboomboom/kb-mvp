94ba3223c7731700254921a77eda50d55e3cd85d
keboom <wangk@cogent-technologies.net>
2024-01-25 14:42:11 +0800

    feat: first beautiful page!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-4xl font-bold text-center">This is my keboom MVP!!! </p>
+       <div className="text-5xl font-extrabold">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 ">
+          Keboom MVP!
+        </span>
+      </div>
      
     </main>
   );
